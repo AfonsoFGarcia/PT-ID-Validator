b507ae213a04398d5d052bbc953aadb182364bb5
Afonso Garcia <afonsogarcia@icloud.com>
2014-04-18 11:24:48 +0100

    Modified the implementation of getNumberFromChar to use ASCII characters and parseInt instead of switch case

--- validator.js
+++ validator.js
@@ -12,6 +12,15 @@ function replaceAll(find, replace, str) {
 }
 
 /*******************************************
+ *         CHAR RELATED FUNCTIONS          *
+ *******************************************/
+
+function isNumber(char) {
+    var asciiVal = char.charCodeAt(0);
+    return (asciiVal >= 48 && asciiVal <=57);
+}
+
+/*******************************************
  *            BI CHECK FUNCTIONS           *
  *******************************************/
 
@@ -53,44 +62,10 @@ function checkBI(biVal) {
  *******************************************/
 
 function getNumberFromChar(letter) {
-    switch(letter) {
-        case '0' : return 0;
-        case '1' : return 1;
-        case '2' : return 2;
-        case '3' : return 3;
-        case '4' : return 4;
-        case '5' : return 5;
-        case '6' : return 6;
-        case '7' : return 7;
-        case '8' : return 8;
-        case '9' : return 9;
-        case 'A' : return 10;
-        case 'B' : return 11;
-        case 'C' : return 12;
-        case 'D' : return 13;
-        case 'E' : return 14;
-        case 'F' : return 15;
-        case 'G' : return 16;
-        case 'H' : return 17;
-        case 'I' : return 18;
-        case 'J' : return 19;
-        case 'K' : return 20;
-        case 'L' : return 21;
-        case 'M' : return 22;
-        case 'N' : return 23;
-        case 'O' : return 24;
-        case 'P' : return 25;
-        case 'Q' : return 26;
-        case 'R' : return 27;
-        case 'S' : return 28;
-        case 'T' : return 29;
-        case 'U' : return 30;
-        case 'V' : return 31;
-        case 'W' : return 32;
-        case 'X' : return 33;
-        case 'Y' : return 34;
-        case 'Z' : return 35;
-        default: return -1;
+    if(isNumber(letter)) {
+        return parseInt(letter);
+    } else {
+        return letter.charCodeAt(0) - 55;
     }
 }
 
